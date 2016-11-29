7ff98d39b188fa63e0da0df0cf119231d7158416
Italo Lourenço <italolt10@gmail.com>
2016-11-29 13:43:52 -0200

    new sql

--- sql/scriptCreate.sql
+++ sql/scriptCreate.sql
@@ -5,14 +5,14 @@ CREATE TABLE IF NOT EXISTS instituicao (
   estado VARCHAR(45) NULL,
   telefone VARCHAR(45) NULL,
   site VARCHAR(45) NULL,
-  PRIMARY KEY (id_instituicao))
+  PRIMARY KEY (id_instituicao));
 
 CREATE TABLE IF NOT EXISTS Patente(
   id_patente SERIAL,
   nome VARCHAR(45) NULL,
   pontuacao_min INT NULL,
   pontuacao_max INT NULL,
-  PRIMARY KEY (id_patente))
+  PRIMARY KEY (id_patente));
   
   
 CREATE TABLE IF NOT EXISTS nivel(
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS nivel(
   nome VARCHAR(45) NULL,
   pontuacaototal INT NULL,
   descricaotema TEXT NULL,
-  PRIMARY KEY (id_nivel))
+  PRIMARY KEY (id_nivel));
 
   
 CREATE TABLE IF NOT EXISTS tarefa (
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS tarefa (
   pontuacao_max INT NULL,
   descricao TEXT NULL,
   id_nivel INT NOT NULL,
-  PRIMARY KEY (id_tarefa,id_nivel),
+  PRIMARY KEY (id_tarefa),
   FOREIGN KEY (id_nivel)
-  REFERENCES nivel(id_nivel))
+  REFERENCES nivel(id_nivel));
 
 
 CREATE TABLE IF NOT EXISTS atividade (
@@ -47,24 +47,10 @@ CREATE TABLE IF NOT EXISTS atividade (
   respostaCerta INT NULL,
   id_tarefa INT NOT NULL,
   id_nivel INT NOT NULL,
-  PRIMARY KEY (id_atividade, id_tarefa, id_nivel),
-  FOREIGN KEY (id_tarefa , id_nivel)
-  REFERENCES tarefa(id_tarefa , id_nivel))
+  PRIMARY KEY (id_atividade),
+  FOREIGN KEY (id_nivel) REFERENCES nivel(id_nivel),
+  FOREIGN KEY (id_tarefa) REFERENCES tarefa(id_tarefa));
   
-  
- 
-
-
-CREATE TABLE IF NOT EXISTS usuario_atividade (
-  id_usuario SERIAL,
-  id_atividade SERIAL,
-  data_realizada DATE NULL,
-  status INT NULL,
-  PRIMARY KEY (id_usuario, id_atividade),
-  FOREIGN KEY (id_usuario)
-  REFERENCES usuario(id_usuario),
-  FOREIGN KEY (id_atividade)
-  REFERENCES atividade(id_atividade))
    
 CREATE TABLE IF NOT EXISTS usuario(
   id_usuario SERIAL,
@@ -79,10 +65,16 @@ CREATE TABLE IF NOT EXISTS usuario(
   pontuacao INT NULL,
   id_instituicao INT NOT NULL,
   id_nivel INT NOT NULL,
-  PRIMARY KEY (id_usuario, id_patente, id_instituicao, id_nivel),
-  FOREIGN KEY (id_patente)
-  REFERENCES Patente(id_patente),
-  FOREIGN KEY (id_instituicao)
-  REFERENCES instituicao(id_instituicao),
-  FOREIGN KEY (id_nivel)
-  REFERENCES nivel(id_nivel))
+  PRIMARY KEY (id_usuario),
+  FOREIGN KEY (id_patente ) REFERENCES Patente(id_patente),
+  FOREIGN KEY (id_instituicao) REFERENCES instituicao(id_instituicao),
+  FOREIGN KEY (id_nivel) REFERENCES nivel(id_nivel));
+
+  CREATE TABLE IF NOT EXISTS usuario_atividade (
+  id_usuario SERIAL,
+  id_atividade SERIAL,
+  data_realizada DATE NULL,
+  status INT NULL,
+  PRIMARY KEY (id_usuario, id_atividade),
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_atividade) REFERENCES atividade(id_atividade));
